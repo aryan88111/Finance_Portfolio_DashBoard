551fe0020b37178d46f71039e74c132048efbd83
Aryan Gautam <76144922+aryan88111@users.noreply.github.com>
2025-05-19 12:46:12 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const stockRoutes = require("./routes/stockRoutes");
-const { getAllStockData } = require("./controllers/stockController");
-const apicache = require("apicache");
 
-const rateLimit = require('express-rate-limit');
-
-
-
-
-let cache = apicache.middleware
-
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-
-})
 
 
 
@@ -30,7 +14,7 @@ app.use(express.json());
 
 
 app.use("/api/stocks", stockRoutes);
-app.use("/", limiter, cache('15 seconds'), getAllStockData);
+
 
 
 const PORT =  5000;
